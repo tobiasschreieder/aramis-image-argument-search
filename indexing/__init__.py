4aec9ce216bfebab7036d81900fb3b3ce2e827ff
Jan Braker <braker.jan@icloud.com>
2021-11-20 14:15:25 +0100

    restructure index
    (cherry picked from commit 89f4eff028e74060b3f0e404064e7853bedeaf56)

--- indexing/__init__.py
+++ indexing/__init__.py
@@ -1,3 +1,5 @@
 from .data_entry import DataEntry, WebPage
 from .index import Index
 from .preprocessing import Preprocessor, SpacyPreprocessor, get_preprocessor
+from .standard_index import StandardIndex
+from .topic_index import TopicQueryIndex
--- startup.py
+++ startup.py
@@ -3,8 +3,7 @@ import logging
 import os
 from logging.handlers import TimedRotatingFileHandler
 
-from frontend import start_server
-from indexing import Index
+from indexing import StandardIndex
 from retrieval import RetrievalSystem
 from retrieval.argument import ArgumentModel
 from retrieval.stance import StanceModel
@@ -44,7 +43,7 @@ def init_logging():
 def index_creation(max_images: int) -> None:
     log.info('Start index creation')
     then = datetime.datetime.now()
-    Index.create_index(max_images).save()
+    StandardIndex.create_index(max_images).save()
     dur = datetime.datetime.now() - then
     log.info('Time for index creation %s', dur)
 
