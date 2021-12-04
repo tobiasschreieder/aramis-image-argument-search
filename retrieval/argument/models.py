4176f00554052acc6b6ff82d8cac93971a832a7f
Jan Braker <braker.jan@icloud.com>
2021-12-04 16:58:48 +0100

    added standard models for argument and stance

--- retrieval/argument/models.py
+++ retrieval/argument/models.py
@@ -1,13 +1,14 @@
 import logging
+import math
 from typing import List, Tuple
 
-from indexing import TermIndex
+from indexing import FeatureIndex
 
 
 class ArgumentModel:
     log = logging.getLogger('ArgumentModel')
 
-    def __init__(self, index: TermIndex):
+    def __init__(self, index: FeatureIndex):
         """
         Constructor for model base class,
         :param index: index to get relevance data from
@@ -34,4 +35,49 @@ class ArgumentModel:
         :return: list of (doc_id, score) tuples descending by score for all documents in the vector space
         """
         self.log.debug('start argument process for query %s', query)
-        return topic_relevant
+        scores = {}
+        if top_k < 0:
+            top_k = len(self.index)
+        else:
+            top_k = min(len(self.index), top_k)
+        for doc_id, topic_score in topic_relevant:
+            scores[doc_id] = self.score(query, doc_id)
+        self.log.debug('scoring done, start sorting')
+        return sorted(scores.items(), key=lambda item: item[1], reverse=True)[:top_k]
+
+
+class StandardArgumentModel(ArgumentModel):
+    log = logging.getLogger('StandardArgumentModel')
+
+    def score(self, query: List[str], doc_id: str) -> float:
+        """
+        Calculates the argument score for a document (given by index and doc_id) and query (give ans query term list)
+        :param query: preprocessed query in list representation to calculate the relevance for
+        :param doc_id: document to calculate the relevance for
+        :return: argument score
+        """
+        image_roi_area = self.index.get_image_roi_area(doc_id)
+        # use cazy function to get a score between 0 and 1 with optimum near 0.8
+        diagramm_factor = self.log_normal_density_function(image_roi_area)
+
+        image_text_sentiment_score = self.index.get_image_text_sentiment_score(doc_id)
+        image_text_len = self.index.get_image_text_len(doc_id)
+        # between 1 and 3 (above 80 ~3)
+        len_words_value = 3 + (((-1) / (math.exp(0.04 * image_text_len))) * 2)
+        text_sentiment_factor = len_words_value * abs(image_text_sentiment_score)
+
+        # (number words - value) [0 - 0][40 - 1][110 - 2][asymptotisch 3]
+        text_factor = (1 - (1 / (math.exp(0.01 * image_text_len)))) * 3
+
+        score = diagramm_factor + text_sentiment_factor + text_factor
+        return score
+
+    @staticmethod
+    def log_normal_density_function(x: float) -> float:
+        if x == 0:
+            return 0
+        elif x == 1:
+            return 0
+        else:
+            return ((1 / (math.sqrt(2 * math.pi) * 0.16 * (-x + 1))) * math.exp(
+                ((math.log((-x + 1), 10) + 0.49) ** 2) / -0.0512) * 0.12)
--- retrieval/stance/models.py
+++ retrieval/stance/models.py
@@ -1,18 +1,19 @@
 import logging
+import math
 from typing import List, Tuple
 
-from indexing import TermIndex
+from indexing import FeatureIndex, ImageType
 
 
 class StanceModel:
     log = logging.getLogger('StanceModel')
 
-    def __init__(self, index: TermIndex):
+    def __init__(self, index: FeatureIndex):
         """
         Constructor for model base class,
         :param index: index to get relevance data from
         """
-        self.index = index
+        self.index: FeatureIndex = index
 
     def score(self, query: List[str], doc_id: str) -> float:
         """
@@ -35,4 +36,69 @@ class StanceModel:
          tuples descending by score for all documents in the vector space
         """
         self.log.debug('start stance process for query %s', query)
-        return argument_relevant, argument_relevant
+        pro_scores = {}
+        con_scores = {}
+        if top_k < 0:
+            top_k = len(self.index)
+        else:
+            top_k = min(len(self.index), top_k)
+        for doc_id, arg_score in argument_relevant:
+            score = self.score(query, doc_id)
+            if score > 0:
+                pro_scores[doc_id] = score
+            elif score < 0:
+                con_scores[doc_id] = score
+
+        self.log.debug('scoring done, start sorting')
+        return \
+            sorted(pro_scores.items(), key=lambda item: item[1], reverse=True)[:top_k], \
+            sorted(con_scores.items(), key=lambda item: item[1], reverse=False)[:top_k]
+
+
+class StandardStanceModel(StanceModel):
+    log = logging.getLogger('StandardStanceModel')
+
+    def score(self, query: List[str], doc_id: str) -> float:
+        """
+        Calculates the stance score for a document (given by index and doc_id) and query (give ans query term list)
+        :param query: preprocessed query in list representation to calculate the relevance for
+        :param doc_id: document to calculate the relevance for
+        :return: stance score
+        """
+        image_type = self.index.get_image_type(doc_id)
+
+        percentage_green = self.index.get_image_percentage_green(doc_id)
+        percentage_red = self.index.get_image_percentage_red(doc_id)
+        percentage_bright = self.index.get_image_percentage_bright(doc_id)
+        percentage_dark = self.index.get_image_percentage_dark(doc_id)
+        image_average_color = self.index.get_image_average_color(doc_id)
+
+        distance_to_green = math.sqrt(
+            (image_average_color[0] - 0) ** 2 + (image_average_color[1] - 255) ** 2 + (image_average_color[2] - 0) ** 2)
+        distance_to_red = math.sqrt(
+            (image_average_color[0] - 255) ** 2 + (image_average_color[1] - 0) ** 2 + (image_average_color[2] - 0) ** 2)
+        distance_to_black = math.sqrt(
+            (image_average_color[0] - 0) ** 2 + (image_average_color[1] - 0) ** 2 + (image_average_color[2] - 0) ** 2)
+        distance_to_white = math.sqrt(
+            (image_average_color[0] - 255) ** 2 + (image_average_color[1] - 255) ** 2 + (
+                    image_average_color[2] - 255) ** 2)
+
+        # between 0 and 1 (above 30 ~1)
+        color_mood = 0
+        if image_type == ImageType.CLIPART:
+            color_mood = (percentage_green * (100 / distance_to_green)) - (percentage_red * (100 / distance_to_red))
+        elif image_type == ImageType.PHOTO:
+            hue_factor = 0.2
+            color_mood = ((percentage_green * (100 / distance_to_green)) -
+                          (percentage_red * (100 / distance_to_red))) + hue_factor * \
+                         ((percentage_bright * (100 / distance_to_white)) -
+                          (percentage_dark * (100 / distance_to_black)))
+
+        image_text_len = self.index.get_image_text_len(doc_id)
+        # between 1 and 0 (above 80 ~0)
+        len_words = 1 / (math.exp(0.04 * image_text_len))
+
+        image_text_sentiment_score = self.index.get_image_text_sentiment_score(doc_id)
+
+        score = (color_mood * len_words) + (image_text_sentiment_score * (1 - len_words))
+        return score
