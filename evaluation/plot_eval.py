precision corrected

evaluation/plot_eval.py
@@ -66,8 +66,11 @@ def plot_scoring_eval(model, topics: List[int],
         cols = 5
 
         for el in [36, 45, 37, 43]:
-            topics.remove(el)
-        if len(topics) > 12:
+            try:
+                topics.remove(el)
+            except ValueError:
+                pass
+        if len(topics) > 20:
             topics = topics[:20]
         print('Cant plot more than 20 topics in one plot, tried %s', len(topics))
 
@@ -77,7 +80,7 @@ def plot_scoring_eval(model, topics: List[int],
     sub_titel = []
     for t in topics:
         topic_title = Topic.get(t).title
-        topic_title = (topic_title[:37] + '..') if len(topic_title) > 40 else topic_title
+        topic_title = (topic_title[:27] + '..') if len(topic_title) > 30 else topic_title
         sub_titel.append(f'Topic {Topic.get(t).number} - {topic_title}')
     fig = make_subplots(rows=rows, cols=cols, shared_xaxes=True, x_title='Score', y_title='Count',
                         vertical_spacing=0.05, horizontal_spacing=0.01, subplot_titles=sub_titel)
@@ -174,18 +177,21 @@ def plot_stance_confusion(model, topics: List[int]) -> go.Figure:
     elif len(topics) <= 12:
         rows = 3
         cols = 4
+    elif len(topics) <= 20:
+        rows = 4
+        cols = 5
     else:
-        rows = 3
-        cols = 4
+        rows = 4
+        cols = 5
 
         for el in [36, 45, 37, 43]:
             try:
                 topics.remove(el)
-            except:
+            except ValueError:
                 pass
-        if len(topics) > 12:
-            topics = topics[:12]
-        print('Cant plot more than 12 topics in one plot, tried %s', len(topics))
+        if len(topics) > 20:
+            topics = topics[:20]
+        print('Cant plot more than 20 topics in one plot, tried %s', len(topics))
 
     infos = ('stance', 'Stance', ('PRO', plotly_color[2]), ('NEUTRAL', plotly_color[0]),
              ('CON', plotly_color[1]), 'color_mood, image_text_sentiment, html_sentiment')
@@ -193,13 +199,16 @@ def plot_stance_confusion(model, topics: List[int]) -> go.Figure:
     sub_titel = []
     for t in topics:
         topic_title = Topic.get(t).title
-        topic_title = (topic_title[:37] + '..') if len(topic_title) > 40 else topic_title
+        topic_title = (topic_title[:30] + '..') if len(topic_title) > 32 else topic_title
         sub_titel.append(f'Topic {Topic.get(t).number} - {topic_title}')
     fig = make_subplots(rows=rows, cols=cols, shared_yaxes=True, shared_xaxes=True, x_title='True Eval',
                         y_title='Predicted Eval',
                         vertical_spacing=0.06, horizontal_spacing=0.02, subplot_titles=sub_titel)
     a = np.array(topics)
     a.resize((rows, cols), refcheck=False)
+
+    avg_error = 0
+    avg_count = 0
 
     for row in range(rows):
         for col in range(cols):
@@ -228,12 +237,17 @@ def plot_stance_confusion(model, topics: List[int]) -> go.Figure:
                 count = df.loc[(df['value'] == eval_val), 'value'].count()
                 for i, score in enumerate(y):
                     z[i][j] = df.loc[(df['value'] == eval_val) & (df['score'] == score), 'value'].count() / count
+                    if i != j:
+                        avg_count += 1
+                        avg_error += z[i][j]
             z.reverse()
             y = x.copy()
             y.reverse()
             fig.add_heatmap(z=z, x=x, y=y, texttemplate='%{z:.4f}', colorscale='Blues', showscale=False,
                             row=row + 1, col=col + 1,)
 
-    fig.update_layout(title=f'{infos[1]} Scoring Confusion matrix')
+    avg_error /= avg_count
+    avg_error *= 2
+    fig.update_layout(title=f'{infos[1]} Scoring Confusion matrix<br><sup>Avg Error: {round(avg_error, 4)}</sup>')
 
     return fig

indexing/neural_net/arg_network.py
@@ -1,3 +1,4 @@
+import abc
 import os
 from pathlib import Path
 from typing import List
@@ -18,14 +19,7 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 pd.options.mode.chained_assignment = None
 
 
-class NArgumentModel_v3:
-    """
-    - skips topics and uses only the best
-    - multiple Networks together (using KERAS Network API)
-        -- One Network for Textposition-Heatmap
-        -- One Network for Color
-        -- One Network combining the two above and the primary-features
-    """
+class NArgumentModel(abc.ABC):
 
     model: keras.Model
     name: str
@@ -36,14 +30,44 @@ class NArgumentModel_v3:
         self.name = name
         self.topics_to_skip = [15, 31, 36, 37, 43, 45, 48]
 
-    @classmethod
-    def load(cls, name: str) -> 'NArgumentModel_v3':
-        arg_model = cls(name)
+    @staticmethod
+    def get(name: str, version: int = 3) -> 'NArgumentModel':
+        if version == 1:
+            return NArgumentModelV1(name)
+        elif version == 2:
+            return NArgumentModelV2(name)
+        else:
+            return NArgumentModelV3(name)
+
+    @staticmethod
+    def load(name: str, version: int = 3) -> 'NArgumentModel':
+        arg_model = NArgumentModel.get(name, version)
         model_path = arg_model.dir_path.joinpath(name).joinpath('model.hS')
         if not model_path.exists():
             raise FileNotFoundError(f'The model {name} does not exists.')
         arg_model.model = load_model(model_path.as_posix(), compile=False)
         return arg_model
+
+    def train(self, data: pd.DataFrame, test: List[int]) -> None:
+        pass
+
+    def predict(self, data: pd.DataFrame) -> List[float]:
+        pass
+
+
+class NArgumentModelV3(NArgumentModel):
+    """
+    - skips topics and uses only the best
+    - multiple Networks together (using KERAS Network API)
+        -- One Network for Textposition-Heatmap
+        -- One Network for Color
+        -- One Network combining the two above and the primary-features
+    """
+
+    def __init__(self, name: str):
+        super().__init__(name)
+        self.dir_path = self.dir_path.joinpath('version_3')
+        self.dir_path.mkdir(parents=True, exist_ok=True)
 
     def train(self, data: pd.DataFrame, test: List[int]) -> None:
         data = data.loc[~data['topic'].isin(self.topics_to_skip)]
@@ -90,27 +114,15 @@ class NArgumentModel_v3:
         return [val[0] for val in predictions]
 
 
-class NArgumentModel_v2:
+class NArgumentModelV2(NArgumentModel):
     """
     - Just one fully connected Network with all features
     """
 
-    model: keras.Model
-    name: str
-    dir_path: Path = Path('index/models/arg/')
-
     def __init__(self, name: str):
+        super().__init__(name)
+        self.dir_path = self.dir_path.joinpath('version_2')
         self.dir_path.mkdir(parents=True, exist_ok=True)
-        self.name = name
-
-    @classmethod
-    def load(cls, name: str) -> 'NArgumentModel_v2':
-        arg_model = cls(name)
-        model_path = arg_model.dir_path.joinpath(name).joinpath('model.hS')
-        if not model_path.exists():
-            raise FileNotFoundError(f'The model {name} does not exists.')
-        arg_model.model = load_model(model_path.as_posix(), compile=False)
-        return arg_model
 
     def train(self, data: pd.DataFrame, test: List[int]) -> None:
         df_train, df_test = split_data(data, test)
@@ -156,16 +168,14 @@ class NArgumentModel_v2:
         return [val[0] for val in predictions]
 
 
-class NArgumentModel_v1:
+class NArgumentModelV1(NArgumentModel):
     """
     - Just one fully connected Network with basic features
     """
 
-    model: keras.Model
-    name: str
-    dir_path: Path = Path('index/models/arg/')
-
     def __init__(self, name: str):
+        super().__init__(name)
+        self.dir_path = self.dir_path.joinpath('version_1')
         self.dir_path.mkdir(parents=True, exist_ok=True)
         self.name = name
         self.cols_to_get_color = [
@@ -185,22 +195,10 @@ class NArgumentModel_v1:
             'image_roi_area'
         ]
 
-    @classmethod
-    def load(cls, name: str) -> 'NArgumentModel_v1':
-        arg_model = cls(name)
-        model_path = arg_model.dir_path.joinpath(name).joinpath('model.hS')
-        if not model_path.exists():
-            raise FileNotFoundError(f'The model {name} does not exists.')
-        arg_model.model = load_model(model_path.as_posix(), compile=False)
-        return arg_model
-
     def train(self, data: pd.DataFrame, test: List[int]) -> None:
         df_train, df_test = split_data(data, test)
         y_train = np.asarray(df_train['arg_eval'])
         y_test = np.asarray(df_test['arg_eval'])
-
-        color_in_train = get_color_data(df_train, cols_to_get=self.cols_to_get_color)
-        color_in_test = get_color_data(df_test, cols_to_get=self.cols_to_get_color)
 
         primary_in_train = get_primary_arg_data(df_train, cols_to_get=self.cols_to_get_primary)
         primary_in_test = get_primary_arg_data(df_test, cols_to_get=self.cols_to_get_primary)
@@ -208,8 +206,8 @@ class NArgumentModel_v1:
         x_train = []
         x_test = []
         for i in range(len(primary_in_train)):
-            x_train.append(color_in_train[i] + primary_in_train[i])
-            x_test.append(color_in_test[i] + primary_in_test[i])
+            x_train.append(self.cols_to_get_color[i] + primary_in_train[i])
+            x_test.append(self.cols_to_get_color[i] + primary_in_test[i])
 
         model = Sequential()
         model.add(Dense(10, input_dim=len(x_train), activation="relu"))